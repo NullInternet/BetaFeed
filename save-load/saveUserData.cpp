fa8529d06f1b54874cf9e845c5734cbd815a430a
NullInternet <ttandtran@gmail.com>
2017-11-15 19:33:23 -0800

    Added description at start of files

--- save-load/saveUserData.cpp
+++ save-load/saveUserData.cpp
@@ -8,25 +8,31 @@
 * This file inserts the total number of likes and other stats
 * of each user into a pre-made table.
 */
-#include "oauth2.h"
 #include <sqlite3.h>
+#include "oauth2.h"
+#include "saveUserData.h"
 
-sqlite3 *db;
-char *sql;
+// Create table for user data upon creating class instance
+SaveUserData::SaveUserData() {
+	sqlite3_open("fbUserData.db", &db);
+	sqlCmd = "CREATE TABLE USERS ("
+		"ID INT PRIMARY KEY NOT NULL,"
+		"LIKES INT NOT NULL DEFAULT (0),";
+		"LOVE INT NOT NULL DEFAULT (0),";
+		"WOW INT NOT NULL DEFAULT (0),";
+		"HAHA INT NOT NULL DEFAULT (0),";
+		"SAD INT NOT NULL DEFAULT (0),";
+		"ANGRY INT NOT NULL DEFAULT (0),";
+		"THANKFUL INT NOT NULL DEFAULT (0));";
+	sqlite3_exec(db, sqlCmd, NULL, 0, NULL);
+}
 
-sqlite3_open("fbUserData.db", &db);
-sqlCmd = "CREATE TABLE USERS ("
-	"ID INT PRIMARY KEY NOT NULL,"
-	"LIKES INT NOT NULL DEFAULT (0),";
-	"LOVE INT NOT NULL DEFAULT (0),";
-	"WOW INT NOT NULL DEFAULT (0),";
-	"HAHA INT NOT NULL DEFAULT (0),";
-	"SAD INT NOT NULL DEFAULT (0),";
-	"ANGRY INT NOT NULL DEFAULT (0),";
-	"THANKFUL INT NOT NULL DEFAULT (0));";
-sqlite3_exec(db, sqlCmd, NULL, 0, NULL);
+// Close database upon deleting class instance
+SaveUserData::~SaveUserData() {
+	sqlite3_close(db);
+}
 
-void saveToDB(sqlite3 *db, char *sql) {
+void SaveUserData::saveToDB() {
 	for (int i = 0; i < OAuth2::users.size(); i++) {
 		sql = "INSERT INTO USERS (ID, LIKES, LOVE, WOW, HAHA, SAD, ANGRY, THANKFUL)"
 			"VALUES(" + OAuth2::users[i].id + ", " // ID
@@ -40,5 +46,3 @@ void saveToDB(sqlite3 *db, char *sql) {
 		sqlite3_exec(db, sql, NULL, 0, NULL);
 	}
 }
-
-sqlite3_close(db);
